274c33bf00cc294cc76ae7a9eec0c3ce7b1e4263
AndrielFR <andrielkogama2@gmail.com>
2020-10-29 13:57:09 -0300

    ginkgo: Switch to AOSP RIL
    Signed-off-by: AndrielFR <andrielkogama2@gmail.com>
    Signed-off-by: andrraa <andraramadanp@outlook.com>

--- du.mk
+++ du.mk
@@ -16,7 +16,7 @@
 
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
